12e8ce90f6557fdb6c80fa96e406406fdd237784
adisigit <adi.sigit13@gmail.com>
2026-08-25 10:18:44 +0700

    add handler and router for cli client login

--- internal/server/router/auth.go
+++ internal/server/router/auth.go
@@ -11,4 +11,6 @@ func RegisterAuthRoutes(api huma.API, r *gin.Engine, h *handler.AuthHandler) {
 	r.GET("/auth/:provider", h.Login)
 	r.GET("/logout", h.Logout)
 	r.GET("/auth/:provider/callback", h.Callback)
+	r.POST("/auth/cli/session", h.CreateCLISession)
+	r.GET("/auth/cli/session/:session_id", h.PollCLISession)
 }
